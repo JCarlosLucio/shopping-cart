8bdff7b5e5fc7086b90346db0dee9e56ea2e6083
jcLucio <c.lucio3@gmail.com>
2020-10-08 17:41:37 -0500

    Add Shop component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/Home';
+import Shop from './components/Shop';
 
 function App() {
   return (
     <div>
       <h1>Shopping Cart</h1>
       <Home />
+      <Shop />
     </div>
   );
 }
