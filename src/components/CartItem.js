597f4aeff847ac28ef196249ddb50170d7ce0acd
jcLucio <c.lucio3@gmail.com>
2020-10-09 17:34:00 -0500

    Add handleIncrease/Decrease w/buttons

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CartItem({ id, itemName, imageId, price, qty, deleteCartItem }) {
+function CartItem({
+  id,
+  itemName,
+  imageId,
+  price,
+  qty,
+  deleteCartItem,
+  incQty,
+  decQty,
+}) {
   const handleDelete = () => deleteCartItem(id);
+  const handleIncrease = () => incQty(id);
+  const handleDecrease = () => decQty(id);
+
   return (
     <div>
       <Link to={`/shop/${id}`}>{itemName}</Link>
       <img src={`https://source.unsplash.com/${imageId}/250x350`} alt={id} />
       <p>${price}</p>
-      <p>Quantity: {qty}</p>
+      <p>
+        <button onClick={handleDecrease}>-</button> Quantity: {qty}{' '}
+        <button onClick={handleIncrease}>+</button>
+      </p>
       <button onClick={handleDelete}>Delete</button>
     </div>
   );
