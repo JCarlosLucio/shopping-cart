688521475f3e2105c5a7c60207e5965b6fe7919d
jcLucio <c.lucio3@gmail.com>
2020-10-09 16:53:34 -0500

    Pass deleteCartItem to CartItem

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-function Cart({ items }) {
+function Cart({ items, deleteCartItem }) {
   return (
     <div>
       <h1>CART</h1>
       {items.length > 0 ? (
-        items.map((item) => <CartItem {...item} />)
+        items.map((item) => (
+          <CartItem {...item} deleteCartItem={deleteCartItem} />
+        ))
       ) : (
         <h1>- Your cart is empty -</h1>
       )}
