1febef13818c2ebbea870b7a15d72d86dd7e6ff0
jcLucio <c.lucio3@gmail.com>
2020-10-08 23:02:32 -0500

    Add items loop to render Item components

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Item from './Item';
 
-function Shop() {
+function Shop({ items }) {
   return (
     <div>
       <h1>SHOP</h1>
+      {items.map((item) => <Item key={item.id} {...item} />)}
     </div>
   );
 }
