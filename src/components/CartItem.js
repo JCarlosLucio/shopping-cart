3a785f4fbc502e469773ba3932c56557ce5b6cb6
jcLucio <c.lucio3@gmail.com>
2020-10-09 16:53:54 -0500

    Add handleDelete

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CartItem({ id, itemName, imageId, price, quantity }) {
+function CartItem({ id, itemName, imageId, price, quantity, deleteCartItem }) {
+  const handleDelete = () => deleteCartItem(id);
   return (
     <div>
       <Link to={`/shop/${id}`}>{itemName}</Link>
       <img src={`https://source.unsplash.com/${imageId}/250x350`} alt={id} />
       <p>${price}</p>
       <p>Quantity: {quantity}</p>
-      <button>Delete</button>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
