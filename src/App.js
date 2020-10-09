93250616e5d99b8b6d0e55fe0c9c3b3099d89f80
jcLucio <c.lucio3@gmail.com>
2020-10-09 14:59:17 -0500

    Add Item Route

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Shop from './components/Shop';
+import Item from './components/Item';
 import Cart from './components/Cart';
 import seedItems from './seedItems';
 
 function App() {
+  const findItem = (id) => seedItems.find((item) => item.id === id);
+
   return (
     <Router>
       <div>
@@ -16,6 +19,13 @@ function App() {
           <Route exact path="/shop">
             <Shop items={seedItems} />
           </Route>
+          <Route
+            exact
+            path="/shop/:id"
+            render={(routeProps) => (
+              <Item {...findItem(routeProps.match.params.id)} />
+            )}
+          />
           <Route exact path="/cart" component={Cart} />
         </Switch>
       </div>
