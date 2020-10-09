172ecdab5d8944cd28490ca30c32ebfae1a06f70
jcLucio <c.lucio3@gmail.com>
2020-10-09 17:11:34 -0500

    Update quantity prop to qty

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CartItem({ id, itemName, imageId, price, quantity, deleteCartItem }) {
+function CartItem({ id, itemName, imageId, price, qty, deleteCartItem }) {
   const handleDelete = () => deleteCartItem(id);
   return (
     <div>
       <Link to={`/shop/${id}`}>{itemName}</Link>
       <img src={`https://source.unsplash.com/${imageId}/250x350`} alt={id} />
       <p>${price}</p>
-      <p>Quantity: {quantity}</p>
+      <p>Quantity: {qty}</p>
       <button onClick={handleDelete}>Delete</button>
     </div>
   );
