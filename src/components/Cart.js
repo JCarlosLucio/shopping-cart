ad1bc76e1cb6dfa7b5b844f73bdd5c58977dd8c5
jcLucio <c.lucio3@gmail.com>
2020-10-09 17:33:15 -0500

    Pass incQty/decQty to CartItem

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-function Cart({ items, deleteCartItem }) {
+function Cart({ items, deleteCartItem, incQty, decQty }) {
   return (
     <div>
       <h1>CART</h1>
       {items.length > 0 ? (
         items.map((item) => (
-          <CartItem key={item.id} {...item} deleteCartItem={deleteCartItem} />
+          <CartItem
+            key={item.id}
+            {...item}
+            deleteCartItem={deleteCartItem}
+            incQty={incQty}
+            decQty={decQty}
+          />
         ))
       ) : (
         <h1>- Your cart is empty -</h1>
