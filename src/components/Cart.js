5ce5ed7aab4d892d6a1f718dedb1e7f1f4f2bea6
jcLucio <c.lucio3@gmail.com>
2020-10-10 16:57:06 -0500

    Refactor cartItems loop

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,18 +2,22 @@ import React from 'react';
 import CartItem from './CartItem';
 
 function Cart({ items, deleteCartItem, changeQty }) {
+  const cartItems = items.map((item) => (
+    <CartItem
+      key={item.id}
+      {...item}
+      deleteCartItem={deleteCartItem}
+      changeQty={changeQty}
+    />
+  ));
+
   return (
     <div>
       <h1>CART</h1>
       {items.length > 0 ? (
-        items.map((item) => (
-          <CartItem
-            key={item.id}
-            {...item}
-            deleteCartItem={deleteCartItem}
-            changeQty={changeQty}
-          />
-        ))
+        <div>
+          <div>{cartItems}</div>
+        </div>
       ) : (
         <h1>- Your cart is empty -</h1>
       )}
