c456dc31e8ed01e1a79e6b1994bad1c49842f38c
jcLucio <c.lucio3@gmail.com>
2020-10-10 22:48:35 -0500

    Add root style

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './ShopItem.module.css';
 
 function Item({ id, imageId, itemName, price }) {
   return (
-    <div>
-      <Link to={`/shop/${id}`}>{itemName}</Link>
+    <div className={styles.root}>
       <Link to={`/shop/${id}`}>
         <img src={`https://source.unsplash.com/${imageId}/350x450`} alt={id} />
       </Link>
+      <Link to={`/shop/${id}`}>{itemName}</Link>
       <p>${price}</p>
     </div>
   );
