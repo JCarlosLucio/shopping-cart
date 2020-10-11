b3d3fa881085e867e4d77c49aaaeabb4673e668f
jcLucio <c.lucio3@gmail.com>
2020-10-10 22:39:56 -0500

    Add root style

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ShopItem from './ShopItem';
+import styles from './Shop.module.css';
 
 function Shop({ items }) {
   return (
-    <div>
-      <h1>SHOP</h1>
+    <div className={styles.root}>
       {items.map((item) => <ShopItem key={item.id} {...item} />)}
     </div>
   );
