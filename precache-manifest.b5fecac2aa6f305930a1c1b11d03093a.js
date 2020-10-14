10abea10d28c7a2825a3d507ea94545b4f3c731e
jcLucio <c.lucio3@gmail.com>
2020-10-13 22:34:09 -0500

    Updates

---
+++ precache-manifest.b5fecac2aa6f305930a1c1b11d03093a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a21484b94aaed1d7e2db24e3243570c",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "3eb6c07f382b062afd96",
+    "url": "/shopping-cart/static/css/main.7f9d9535.chunk.css"
+  },
+  {
+    "revision": "7abdaf77ff819fec8e5e",
+    "url": "/shopping-cart/static/js/2.78cdedc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.78cdedc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb6c07f382b062afd96",
+    "url": "/shopping-cart/static/js/main.b38d6fbb.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
