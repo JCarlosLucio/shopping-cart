b12afc4b1f82aaff049865036ddc8b7e0a8d9906
jcLucio <c.lucio3@gmail.com>
2020-10-13 23:02:05 -0500

    Update Router to use HashRouter for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Shop from './components/Shop';
@@ -36,7 +36,7 @@ function App() {
   const cartItemsQty = cartItems.reduce((acc, cur) => acc + cur.qty, 0);
 
   return (
-    <Router>
+    <Router basename="/">
       <React.Fragment>
         <Navbar cartItemsQty={cartItemsQty} />
         <Switch>
